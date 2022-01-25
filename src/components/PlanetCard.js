59e2dfd22c435ef246fccf4b5aeb476010be22cd
Caio <caiopadilha@id.uff.br>
2022-01-25 17:20:18 -0300

    Iniciando requisito 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">{ PlanetCard }</div>
+    );
+  }
+}
